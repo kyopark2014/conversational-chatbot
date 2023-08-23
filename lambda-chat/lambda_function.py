e916854a10bb381d2dc8fc069e6eccc58b330a68
ksdyb.park <ksdyb@amazon.com>
2023-08-23 21:47:49 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -101,48 +101,6 @@ llm = Bedrock(model_id=modelId, client=boto3_bedrock, model_kwargs=parameters)
 
 history = []
 def get_answer_using_template(query):    
-    #prompt_template = """
-    #    The following is a friendly conversation between a human and an AI. 
-    #    The AI is talkative and provides lots of specific details from its context.
-    #    If the AI does not know the answer to a question, it truthfully says it 
-    #    does not know.
-    #    {context}
-    #    Instruction: Based on the above documents, provide a detailed answer for, {question} Answer "don't know" 
-    #    if not present in the document. 
-    #    Solution:"""
-    #PROMPT = PromptTemplate(
-    #    template=prompt_template, input_variables=["context", "question"],
-    #)
-
-    #condense_qa_template = """
-    #    Given the following conversation and a follow up question, rephrase the follow up question 
-    #    to be a standalone question.
-
-    #    Chat History:
-    #    {chat_history}
-    #    Follow Up Input: {question}
-    #    Standalone question:"""
-    #standalone_question_prompt = PromptTemplate.from_template(condense_qa_template)
-
-
-    #prompt_template = """Human: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
-
-    #{context}
-
-    #Question: {question}
-    #Assistant:"""
-    #PROMPT = PromptTemplate(
-    #    template=prompt_template, input_variables=["context", "question"]
-    #    )
-
-    #template = """The following is a friendly conversation between a human and an AI. The AI is #talkative and provides lots of specific details from its context. If the AI does not know #the answer to a question, it truthfully says it does not know.
-
-    #Current conversation:
-    #{history}
-    #Human: {input}
-    #AI Assistant:"""
-    #PROMPT = PromptTemplate(input_variables=["history", "input"], template=template)
-
     prompt_template = """Human: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
     Current conversation:
@@ -164,6 +122,8 @@ def get_answer_using_template(query):
     history.append('Human: '+query)
     history.append('Assistant: '+result)
 
+    print('history: ', history)
+
     #from langchain import ConversationChain
     #conversation = ConversationChain(
     #    llm=llm, 
@@ -172,11 +132,8 @@ def get_answer_using_template(query):
     #    memory=ConversationBufferMemory(ai_prefix="AI Assistant"),
     #)
     #result = conversation.predict(input=query)
-
     
     return result
-
-
 
 def get_summary(file_type, s3_file_name):
     summary = ''
@@ -291,9 +248,9 @@ def lambda_handler(event, context):
     else:             
         if type == 'text':
             text = body
+
             #msg = llm(text)
             msg = get_answer_using_template(text)
-
             
         elif type == 'document':
             object = body
