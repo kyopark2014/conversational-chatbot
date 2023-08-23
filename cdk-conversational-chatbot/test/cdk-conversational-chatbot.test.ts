3db5631da5e25933c8cad5cda296559ffb527e99
ksdyb.park <ksdyb@amazon.com>
2023-08-23 19:14:13 +0900

    update

---
+++ cdk-conversational-chatbot/test/cdk-conversational-chatbot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkConversationalChatbot from '../lib/cdk-conversational-chatbot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-conversational-chatbot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkConversationalChatbot.CdkConversationalChatbotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
