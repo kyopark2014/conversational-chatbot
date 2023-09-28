161ba39999697b992c7117e7a95ce2bc1435594e
ksdyb.park <ksdyb@amazon.com>
2023-09-29 07:28:21 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -85,6 +85,37 @@ llm = Bedrock(
 
 map = dict() # Conversation
 
+def get_conversation_prompt(query):
+    # check korean
+    pattern_hangul = re.compile('[\u3131-\u3163\uac00-\ud7a3]+')
+    word_kor = pattern_hangul.search(str(query))
+    print('word_kor: ', word_kor)
+
+    if word_kor:    
+        #condense_template = """\n\nHuman: 아래 문맥(context)을 참조했음에도 답을 알 수 없다면, 솔직히 모른다고 말합니다.
+        condense_template = """다음은 Human과 Assistant의 친근한 대화입니다. Assistant은 상황에 맞는 구체적인 세부 정보를 충분히 제공합니다. 아래 문맥(context)을 참조했음에도 답을 알 수 없다면, 솔직히 모른다고 말합니다.
+
+        Current conversation:
+        {history}
+        
+        Human: {input}
+        
+        Assistant:
+        """
+    else:
+        condense_template = """\n\nHuman: Using the following conversation, answer friendly for the newest question. If you don't know the answer, just say that you don't know, don't try to make up an answer. You will be acting as a thoughtful advisor.
+
+        {history}
+        
+        Human: {input}
+
+        Assistant:
+        """
+
+        #claude_prompt = PromptTemplate.from_template("""The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+    
+    return PromptTemplate.from_template(condense_template)
+
 def get_answer_using_chat_history(query, chat_memory):  
     # check korean
     pattern_hangul = re.compile('[\u3131-\u3163\uac00-\ud7a3]+')
@@ -347,22 +378,11 @@ def lambda_handler(event, context):
             else:            
                 if conversationMode == 'true':
                     if methodOfConversation == 'ConversationChain':
-                        #condense_template = """\n\nHuman: 아래 문맥(context)을 참조했음에도 답을 알 수 없다면, 솔직히 모른다고 말합니다.
-                        condense_template = """다음은 Human과 Assistant의 친근한 대화입니다. Assistant은 상황에 맞는 구체적인 세부 정보를 충분히 제공합니다. 아래 문맥(context)을 참조했음에도 답을 알 수 없다면, 솔직히 모른다고 말합니다.
-
-                        Current conversation:
-                        {history}
-        
-                        Human: {input}
-        
-                        Assistant:
-                        """
-
-                        conversation.prompt = PromptTemplate.from_template(condense_template)
+                        conversation.prompt = get_conversation_prompt(text)
 
                         msg = conversation.predict(input=text)
 
-                        # extract chat history
+                        # extract chat history for debug
                         chats = chat_memory.load_memory_variables({})
                         chat_history_all = chats['history']
                         print('chat_history_all: ', chat_history_all)
